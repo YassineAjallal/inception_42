bde1221cc874cd17d288ae9ccc22187a4054af9f
mouad <bousbaamouad@gmail.com>
2023-09-16 18:24:01 +0100

    wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,11 @@
 #!bin/sh
 
 service mysql start;
-sleep 4;
-mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;";
-mysql -e "CREATE USER IF NOT EXISTS 'yassine'@'localhost' IDENTIFIED BY 'yajallal';";
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO yajallal@'%' IDENTIFIED BY 'yajallal';";
+sleep 10;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'yajallal';";
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysqladmin -u root -pyajallal shutdown;
 exec "$@"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,14 +1,20 @@
+sleep 10
+if [ ! -d "/run/php" ]; 
+then
+  mkdir -p "/run/php"
+fi
+
 wp config create	--allow-root \
-					--dbname=${SQL_DATABASE} \
-					--dbuser=${SQL_USER} \
-					--dbpass=${SQL_PASSWORD} \
+					--dbname=${MYSQL_DATABASE} \
+					--dbuser=${MYSQL_USER} \
+					--dbpass=${MYSQL_PASSWORD} \
 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL}
 
 echo "Creating users..."
 wp user create ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress --allow-root
 
-wp user create ${WP_USER} yusufisawii@gmail.com --role=editor --user_pass=${WP_PASSWD} --path=/var/www/html/wordpress --allow-root 
+wp user create ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_USER_PASSWD} --path=/var/www/html/wordpress --allow-root 
 
 
 exec "$@"
