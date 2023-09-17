f7081a4b814cdb4d1fdc73c26a016c3457e60489
mouad <bousbaamouad@gmail.com>
2023-09-17 02:24:07 +0100

    wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,21 @@
 #!bin/sh
 
-service mysql start;
+service mariadb start;
 sleep 10;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" -u root
+mysql -e "CREATE USER IF NOT EXISTS 'yajallal'@'localhost' IDENTIFIED BY 'yajallal';" -u root
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'yajallal'@'%' IDENTIFIED BY 'yajallal';" -u root
 mysql -e "FLUSH PRIVILEGES;";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'yajallal';"
 mysqladmin -u root -pyajallal shutdown;
 exec "$@"
+
+# service mariadb start;
+# sleep 10;
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" -u root
+# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root
+# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root
+# mysql -e "FLUSH PRIVILEGES;"; -u root
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# mysqladmin -u root -pyajallal shutdown;
+# exec "$@"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,14 +4,22 @@ then
   mkdir -p "/run/php"
 fi
 
+chmod -R 755 /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/wordpress
+
+cd /var/www/html/wordpress
+rm -rf /var/www/html/wordpress/*
+
+wp core download --allow-root
+
 wp config create	--allow-root \
 					--dbname=${MYSQL_DATABASE} \
 					--dbuser=${MYSQL_USER} \
 					--dbpass=${MYSQL_PASSWORD} \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+					--dbhost=mariadb:3306
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL}
 
-echo "Creating users..."
+
 wp user create ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress --allow-root
 
 wp user create ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_USER_PASSWD} --path=/var/www/html/wordpress --allow-root 
