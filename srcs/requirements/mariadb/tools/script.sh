15f9c4bf8f42ba552a0db71bd4c3c4cd2ce7cfed
yassine542 <yassineajallal@gmail.com>
2023-09-24 06:22:03 +0000

    adminer

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,6 +2,7 @@
 
 service mariadb start;
 sleep 5;
+mysql -u -p root "USE information_schema; SELECT COUNT(*) as table_count FROM TABLES WHERE TABLE_SCHEMA = 'wordpress';" | grep 0 |
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" -u root
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root
