8d5897342088023a65e8464ff8fd0afb08974651
yassine542 <yassineajallal@gmail.com>
2023-09-25 05:11:06 +0000

    wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,11 @@
 #!bin/sh
 
-service mariadb start;
-sleep 5;
-mysql -u -p root "USE information_schema; SELECT COUNT(*) as table_count FROM TABLES WHERE TABLE_SCHEMA = 'wordpress';" | grep 0 |
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" -u root
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root
-mysql -e "FLUSH PRIVILEGES;";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown;
+service mariadb start; # start mariadb server
+sleep 5; # waiting for mariadb to start properly
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" -u root # create new database
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root # create new user
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root # give all privileges on wordpress database  to new user  
+mysql -e "FLUSH PRIVILEGES;"; # save changes
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" # changes the root password
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown; # shutdown the server
 exec "$@"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,4 +1,6 @@
 sleep 10
+
+# create the socket folder of php
 if [ ! -d "/run/php" ]; 
 then
   mkdir -p "/run/php"
@@ -6,12 +8,9 @@ fi
 
 cd /var/www/html/wordpress
 rm -rf /var/www/html/wordpress/*
-
 wp core download	--allow-root # download core files
 wp config create	--allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 # create the config file of wordpress that contain database info
 wp core install		--allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL} # create new wordpress website
 wp user create		--allow-root ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress # create first user (administrator)
 wp user create		--allow-root ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_USER_PASSWD} --path=/var/www/html/wordpress # create second user (editor)
-
-
 exec "$@"
