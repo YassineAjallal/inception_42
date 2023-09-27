7b6afe023ced5897894339dcc043003eadb894a9
yassine542 <yassineajallal@gmail.com>
2023-09-27 00:52:44 +0000

    protect wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,19 @@
 
 service mariadb start; # start mariadb server
 sleep 5; # waiting for mariadb to start properly
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" -u root # create new database
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root # create new user
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -u root # give all privileges on wordpress database  to new user  
-mysql -e "FLUSH PRIVILEGES;"; # save changes
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" # changes the root password
+# mysql -u root --skip-password;
+
+mysql -u root --skip-password <<EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" # create new database
+# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" # create new user
+# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" # give all privileges on wordpress database  to new user  
+# mysql -u root -p$MYSQL_ROOT_PASSWORD -e  "FLUSH PRIVILEGES;"; # save changes
+# # mysql -e -u root "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" # changes the root password
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown; # shutdown the server
 exec "$@"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,16 +1,22 @@
 sleep 10
 
 # create the socket folder of php
-if [ ! -d "/run/php" ]; 
-then
+if [ ! -d "/run/php" ]; then
   mkdir -p "/run/php"
 fi
 
-cd /var/www/html/wordpress
-rm -rf /var/www/html/wordpress/*
-wp core download	--allow-root # download core files
-wp config create	--allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 # create the config file of wordpress that contain database info
-wp core install		--allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL} # create new wordpress website
-wp user create		--allow-root ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress # create first user (administrator)
-wp user create		--allow-root ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_USER_PASSWD} --path=/var/www/html/wordpress # create second user (editor)
+if ! wp --allow-root core is-installed; then
+	wp core download	--allow-root --path=/var/www/html/wordpress # download core files
+	wp config create	--allow-root --path=/var/www/html/wordpress --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 # create the config file of wordpress that contain database info
+	wp core install		--allow-root --path=/var/www/html/wordpress --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL} # create new wordpress website
+fi
+
+wp user list --allow-root --field=user_login | grep -q ${WP_ADMIN}
+if [ $? != 0 ]; then
+	wp user create --allow-root ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress # create first user (administrator)
+fi
+wp user list --allow-root --field=user_login | grep -q ${WP_USER}
+if [ $? != 0 ]; then
+	wp user create --allow-root ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_USER_PASSWD} --path=/var/www/html/wordpress # create second user (editor)
+fi
 exec "$@"
